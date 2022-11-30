b6519e40b904867a170cce8f5e4acc7021704904
sidghese <sighese@gmail.com>
2022-11-30 15:48:13 -0500

    add data

---
+++ sts-frontend/src/data/navbar/index.js
@@ -0,0 +1,28 @@
+export default [
+    {
+      "id": 1,
+      "title": "Home",
+      "link": "/",
+      "subMenu": false
+    },
+    {
+      "id": 1,
+      "title": "Over ons",
+      "link": "/over-ons",
+      "subMenu": false
+    },
+    {
+      "id": 1,
+      "title": "Projecten",
+      "link": "/projecten",
+      "subMenu": false
+    },
+    {
+      "id": 1,
+      "title": "Contact",
+      "link": "/contact",
+      "subMenu": false
+    }
+  ]
+  
+  
